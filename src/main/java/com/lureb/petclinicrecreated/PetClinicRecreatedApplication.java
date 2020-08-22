3e8c14b6009f2074e73e97b077971e897ddd3726
VrebiC <irebic@outlook.com>
2020-08-22 13:30:21 +0200

    First commit

---
+++ src/main/java/com/lureb/petclinicrecreated/PetClinicRecreatedApplication.java
@@ -0,0 +1,13 @@
+package com.lureb.petclinicrecreated;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicRecreatedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetClinicRecreatedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lureb/petclinicrecreated/PetClinicRecreatedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lureb.petclinicrecreated;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicRecreatedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
