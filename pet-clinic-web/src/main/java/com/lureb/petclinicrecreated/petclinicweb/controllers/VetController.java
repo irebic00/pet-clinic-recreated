a984c815f8fe9a2bb672bf1a852906ed009d0d5b
VrebiC <irebic@outlook.com>
2020-08-23 22:13:14 +0200

    List All Vets on Vets Index Page #15

--- pet-clinic-web/src/main/java/com/lureb/petclinicrecreated/petclinicweb/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/lureb/petclinicrecreated/petclinicweb/controllers/VetController.java
@@ -1,13 +1,25 @@
 package com.lureb.petclinicrecreated.petclinicweb.controllers;
 
+import com.lureb.petclinicrecreated.petclinicdata.services.VetService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    @Autowired
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @GetMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/index";
     }
 }
