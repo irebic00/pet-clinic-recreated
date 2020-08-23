269252bffbad2ce5e4b6f0afda14a7fece6d922b
VrebiC <irebic@outlook.com>
2020-08-23 16:58:31 +0200

    Add BaseEntity with Id. Closes #10

--- pet-clinic-data/src/main/java/com/lureb/petclinicrecreated/petclinicdata/model/Person.java
+++ pet-clinic-data/src/main/java/com/lureb/petclinicrecreated/petclinicdata/model/Person.java
@@ -1,6 +1,6 @@
 package com.lureb.petclinicrecreated.petclinicdata.model;
 
-public class Person {
+public class Person extends BaseEntity {
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/com/lureb/petclinicrecreated/petclinicdata/model/Pet.java
+++ pet-clinic-data/src/main/java/com/lureb/petclinicrecreated/petclinicdata/model/Pet.java
@@ -2,7 +2,7 @@ package com.lureb.petclinicrecreated.petclinicdata.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity {
     private PetType petType;
     private Owner owner;
     private LocalDate birthday;
