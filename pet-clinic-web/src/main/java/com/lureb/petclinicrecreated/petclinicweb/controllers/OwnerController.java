e427c30ff4af1d88e66c380aeec57c97018c30ba
VrebiC <irebic@outlook.com>
2020-08-23 22:07:40 +0200

    List All Owners on Owners Index Page. Closes #16

--- pet-clinic-web/src/main/java/com/lureb/petclinicrecreated/petclinicweb/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/lureb/petclinicrecreated/petclinicweb/controllers/OwnerController.java
@@ -1,13 +1,22 @@
 package com.lureb.petclinicrecreated.petclinicweb.controllers;
 
+import com.lureb.petclinicrecreated.petclinicdata.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @GetMapping({"/owners", "/owners/index", "/owners/index.html"})
-    public String getOwners() {
+    public String getOwners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
