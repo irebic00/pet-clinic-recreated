a890b64563d479e502f505e0bdee8283a9500f64
VrebiC <irebic@outlook.com>
2020-08-23 20:01:34 +0200

    Added owners index page and controller. Closes #13

---
+++ pet-clinic-web/src/main/java/com/lureb/petclinicrecreated/petclinicweb/controllers/OwnerController.java
@@ -0,0 +1,13 @@
+package com.lureb.petclinicrecreated.petclinicweb.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class OwnerController {
+
+    @GetMapping({"/owners", "/owners/index", "/owners/index.html"})
+    public String getOwners() {
+        return "owners/index";
+    }
+}
