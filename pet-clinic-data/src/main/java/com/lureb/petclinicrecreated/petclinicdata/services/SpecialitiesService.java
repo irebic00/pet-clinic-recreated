083129276f78f4e818a6d7a6ba4624d509d541f5
VrebiC <irebic@outlook.com>
2020-08-25 23:11:32 +0200

    Create Speciality Map Service Impl. Closes #31

---
+++ pet-clinic-data/src/main/java/com/lureb/petclinicrecreated/petclinicdata/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package com.lureb.petclinicrecreated.petclinicdata.services;
+
+import com.lureb.petclinicrecreated.petclinicdata.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
