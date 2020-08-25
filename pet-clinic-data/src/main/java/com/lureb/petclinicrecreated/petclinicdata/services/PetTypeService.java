876e1027644a532a8305129ad49baa9488c19155
VrebiC <irebic@outlook.com>
2020-08-25 18:09:50 +0200

    Create PetType Map Service Impl. Closes #29

---
+++ pet-clinic-data/src/main/java/com/lureb/petclinicrecreated/petclinicdata/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.lureb.petclinicrecreated.petclinicdata.services;
+
+import com.lureb.petclinicrecreated.petclinicdata.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/lureb/petclinicrecreated/petclinicdata/services/map/PetTypeServiceMap.java
@@ -0,0 +1,35 @@
+package com.lureb.petclinicrecreated.petclinicdata.services.map;
+
+import com.lureb.petclinicrecreated.petclinicdata.model.PetType;
+import com.lureb.petclinicrecreated.petclinicdata.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType petType) {
+        super.delete(petType);
+    }
+
+    @Override
+    public PetType save(PetType petType) {
+        return super.save(petType);
+    }
+}
