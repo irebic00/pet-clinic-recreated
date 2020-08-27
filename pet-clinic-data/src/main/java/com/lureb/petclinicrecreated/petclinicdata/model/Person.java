48a0e747aed28e1643130296c80c97704db1af86
VrebiC <irebic@outlook.com>
2020-08-27 14:52:49 +0200

    Converted Owners to JPA entities. Closes #36

--- pet-clinic-data/src/main/java/com/lureb/petclinicrecreated/petclinicdata/model/Person.java
+++ pet-clinic-data/src/main/java/com/lureb/petclinicrecreated/petclinicdata/model/Person.java
@@ -1,7 +1,15 @@
 package com.lureb.petclinicrecreated.petclinicdata.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class Person extends BaseEntity {
+
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
 
     public String getFirstName() {
--- pet-clinic-data/src/main/java/com/lureb/petclinicrecreated/petclinicdata/model/Pet.java
+++ pet-clinic-data/src/main/java/com/lureb/petclinicrecreated/petclinicdata/model/Pet.java
@@ -1,12 +1,24 @@
 package com.lureb.petclinicrecreated.petclinicdata.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
 
+    @Column(name = "name")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @Column(name = "birthday")
     private LocalDate birthday;
 
     public String getName() {
