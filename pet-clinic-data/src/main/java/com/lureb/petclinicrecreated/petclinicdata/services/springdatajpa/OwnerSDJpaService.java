a802a39add343421ba1322277450e3fc2dc152be
VrebiC <irebic@outlook.com>
2020-08-27 15:53:48 +0200

    Created Spring Data JPA Service for Vets. Closes #41

--- pet-clinic-data/src/main/java/com/lureb/petclinicrecreated/petclinicdata/services/springdatajpa/OwnerSDJpaService.java
+++ pet-clinic-data/src/main/java/com/lureb/petclinicrecreated/petclinicdata/services/springdatajpa/OwnerSDJpaService.java
@@ -2,8 +2,6 @@ package com.lureb.petclinicrecreated.petclinicdata.services.springdatajpa;
 
 import com.lureb.petclinicrecreated.petclinicdata.model.Owner;
 import com.lureb.petclinicrecreated.petclinicdata.repositories.OwnerRepository;
-import com.lureb.petclinicrecreated.petclinicdata.repositories.PetRepository;
-import com.lureb.petclinicrecreated.petclinicdata.repositories.PetTypeRepository;
 import com.lureb.petclinicrecreated.petclinicdata.services.OwnerService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
@@ -16,13 +14,9 @@ import java.util.Set;
 public class OwnerSDJpaService implements OwnerService {
 
     private final OwnerRepository ownerRepository;
-    private final PetRepository petRepository;
-    private final PetTypeRepository petTypeRepository;
 
-    public OwnerSDJpaService(OwnerRepository ownerRepository, PetRepository petRepository, PetTypeRepository petTypeRepository) {
+    public OwnerSDJpaService(OwnerRepository ownerRepository) {
         this.ownerRepository = ownerRepository;
-        this.petRepository = petRepository;
-        this.petTypeRepository = petTypeRepository;
     }
 
     @Override
---
+++ pet-clinic-data/src/main/java/com/lureb/petclinicrecreated/petclinicdata/services/springdatajpa/VetSDJpaService.java
@@ -0,0 +1,48 @@
+package com.lureb.petclinicrecreated.petclinicdata.services.springdatajpa;
+
+import com.lureb.petclinicrecreated.petclinicdata.model.Vet;
+import com.lureb.petclinicrecreated.petclinicdata.repositories.VetRepository;
+import com.lureb.petclinicrecreated.petclinicdata.services.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile(value = "springdatajpa")
+public class VetSDJpaService implements VetService {
+
+    private final VetRepository vetRepository;
+
+    public VetSDJpaService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return vetRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+        return vets;
+    }
+
+    @Override
+    public Vet save(Vet vet) {
+        return vetRepository.save(vet);
+    }
+
+    @Override
+    public void delete(Vet vet) {
+        vetRepository.delete(vet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        vetRepository.deleteById(id);
+    }
+}
