358d6ebd8118c3ccff379febac0fc15e2c8fdafb
VrebiC <irebic@outlook.com>
2020-08-27 16:47:37 +0200

    Created Visit Map Service. Closes #46

---
+++ pet-clinic-data/src/main/java/com/lureb/petclinicrecreated/petclinicdata/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.lureb.petclinicrecreated.petclinicdata.services;
+
+import com.lureb.petclinicrecreated.petclinicdata.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
