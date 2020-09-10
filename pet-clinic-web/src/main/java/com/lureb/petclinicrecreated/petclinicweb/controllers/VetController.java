fea0123cda6cc9943d51499d11cec925756d62f3
VrebiC <irebic@outlook.com>
2020-09-10 10:45:42 +0200

    api endpoint for list of vets as json. Closes #75

--- pet-clinic-web/src/main/java/com/lureb/petclinicrecreated/petclinicweb/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/lureb/petclinicrecreated/petclinicweb/controllers/VetController.java
@@ -1,10 +1,14 @@
 package com.lureb.petclinicrecreated.petclinicweb.controllers;
 
+import com.lureb.petclinicrecreated.petclinicdata.model.Vet;
 import com.lureb.petclinicrecreated.petclinicdata.services.VetService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -21,5 +25,10 @@ public class VetController {
         model.addAttribute("vets", vetService.findAll());
 
         return "vets/vetList";
+    }
+
+    @GetMapping({"/api/vets"})
+    public ResponseEntity<Set<Vet>> listVetsJson() {
+        return ResponseEntity.ok(vetService.findAll());
     }
 }
